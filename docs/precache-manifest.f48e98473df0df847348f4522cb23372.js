ff277f3a983f5e35a95ce622be212ccb10181523
Elvira Fuente Romero <elvira.fuente@ultimasoftware.com>
2019-11-05 10:38:21 +0100

    add docs

---
+++ docs/precache-manifest.f48e98473df0df847348f4522cb23372.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a17eb3865df1bf00b19aee05eb6794e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddfe84cc1718d48e458e",
+    "url": "/static/js/2.a9ebe5f4.chunk.js"
+  },
+  {
+    "revision": "7c876030ff349ff77eca",
+    "url": "/static/js/main.178b1bf7.chunk.js"
+  },
+  {
+    "revision": "9ca82872bb3b897ed8e8",
+    "url": "/static/js/runtime-main.b853c010.js"
+  }
+]);
